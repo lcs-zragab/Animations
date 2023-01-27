cb213952a4e114de0e603f6e5a86121f22a587a8
zaid ragab <zragab@lcs.on.ca>
2023-01-27 14:20:15 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Zaid Ragab on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
